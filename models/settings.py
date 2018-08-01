dcaaeb9ae986b8fed75725177cbd1ce20093e57e
Alireza <ar.sedghi@gmail.com>
2018-08-01 15:16:04 -0400

    ENH: added dicom caching ENH: added new model with auc 80

--- models/settings.py
+++ models/settings.py
@@ -5,29 +5,13 @@ import getpass
 folders = []
 intermediate_folder = ''
 
-if socket.gethostname() in ['theBeast', ]:
-    intermediate_folder = os.environ['HOME'] + "/projects/pcad/intermediate"
+if socket.gethostname() == 'pmous008':
+    intermediate_folder = os.environ['HOME'] + "/projects/pcad"
     slicer_dir = os.environ['HOME'] + "/sources/Slicer-4.8.1-linux-amd64"
 
 elif socket.gethostname() == 'pirads-trainer':
     intermediate_folder = os.environ['HOME'] + "/projects/pcad"
     slicer_dir = os.environ['HOME'] + "/sources/Slicer-4.8.1-linux-amd64"
 
-elif socket.gethostname() == 'pmous008':
-    intermediate_folder = os.environ['HOME'] + "/projects/pcad"
-    slicer_dir = os.environ['HOME'] + "/sources/Slicer-4.8.1-linux-amd64"
-
-# data folders
-folders.append(intermediate_folder)
-data_folder = os.path.join(intermediate_folder, 'data')
-folders.append(data_folder)
-raw_folder = os.path.join(data_folder, 'raw')
-folders.append(raw_folder)
-sheets_folder = os.path.join(data_folder, 'sheets')
-folders.append(sheets_folder)
-nrrd_folder = os.path.join(data_folder, 'nrrd')
-folders.append(nrrd_folder)
-split_folder = os.path.join(data_folder, 'split')
-folders.append(split_folder)
-npy_folder = os.path.join(data_folder, 'npy')
-folders.append(npy_folder)
+dicom_folder = intermediate_folder + "/dicom"
+nrrd_folder = intermediate_folder + "/nrrd"
